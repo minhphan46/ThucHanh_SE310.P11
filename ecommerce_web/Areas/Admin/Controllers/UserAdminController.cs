d16a0a1d8431dc6328cb849b61ea9f729a562c5f
Phan Van Minh <21522345@gm.uit.edu.vn>
2024-11-04 13:28:13 +0700

    init source

---
+++ ecommerce_web/Areas/Admin/Controllers/UserAdminController.cs
@@ -0,0 +1,94 @@
+﻿using ecommerce_web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using X.PagedList;
+
+namespace ecommerce_web.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Route("admin")]
+    [Route("admin/useradmin")]
+    public class UserAdminController : Controller
+    {
+        QlBanValiContext db = new QlBanValiContext();
+
+        [Route("AllUsers")]
+        public IActionResult AllUsers(int? page)
+        {
+            int pageSize = 12;
+            int pageNumber = page == null || page < 0 ? 1 : page.Value;
+            var users = db.TUsers.AsNoTracking().OrderBy(x => x.Username);
+            PagedList<TUser> lst = new PagedList<TUser>(users, pageNumber, pageSize);
+            return View(lst);
+        }
+
+        [Route("AddUser")]
+        [HttpGet]
+        public IActionResult AddUser()
+        {
+            return View();
+        }
+
+        [Route("AddUser")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult AddUser(TUser user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TUsers.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("AllUsers");
+            }
+            return View(user);
+        }
+
+        [Route("EditUser")]
+        [HttpGet]
+        public IActionResult EditUser(string username)
+        {
+            var user = db.TUsers.Find(username);
+            return View(user);
+        }
+
+        [Route("EditUser")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult EditUser(TUser user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("AllUsers");
+            }
+            return View(user);
+        }
+
+        [Route("DeleteUser")]
+        [HttpGet]
+        public IActionResult DeleteUser(string username)
+        {
+            TempData["Message"] = "";
+
+            var khachhang = db.TKhachHangs.Where(x => x.Username == username).ToList();
+            if (khachhang.Count() > 0)
+            {
+                TempData["Message"] = "Không xóa được người dùng này";
+                return RedirectToAction("AllUsers", "UserAdmin");
+            }
+
+            var nhanvien = db.TKhachHangs.Where(x => x.Username == username).ToList();
+            if (nhanvien.Count() > 0)
+            {
+                TempData["Message"] = "Không xóa được người dùng này";
+                return RedirectToAction("AllUsers", "UserAdmin");
+            }
+
+            db.Remove(db.TUsers.Find(username));
+            db.SaveChanges();
+            TempData["Message"] = "Người dùng đã được xóa";
+            return RedirectToAction("AllUsers", "UserAdmin");
+        }
+    }
+}
